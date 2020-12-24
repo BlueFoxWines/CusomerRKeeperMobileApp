9d30144dea4dc1a36126a2939204c0cf576fe41e
Anton Zolotov <antonzolotov@outlook.com>
2020-12-24 14:21:24 +0500

    Реализован метод получения токена авторизации

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -48,18 +48,70 @@ const actions = {
                 reject(error)
                 commit(LOGIN_REQUEST_SENDCODE_ERROR, error)
             })
+    }),
+    [LOGIN_REQUEST_TOKEN]: ({ commit }, payload) => new Promise((resolve, reject) => {
+        commit(LOGIN_REQUEST_TOKEN)
+        HTTP({
+            url: "account/login",
+            method: "GET",
+            params: {
+                phone: payload.Phone,
+                code: payload.Code
+            }
+        })
+            .then((response) => {
+                resolve(response)
+                if (response && response.status === 200 && response.data && response.data.token) {
+                    commit(LOGIN_REQUEST_TOKEN_SUCCESS, response.data)
+                }
+                else {
+                    // Helpers.notify(
+                    //     "is-danger",
+                    //     i18n.t("Message.Backend.NoData")
+                    // )
+                }
+            })
+            .catch((error) => {
+                reject(error)
+                commit(LOGIN_REQUEST_TOKEN_ERROR, error)
+            })
     })
 }
 
 const mutations = {
     [LOGIN_REQUEST_SENDCODE]: (state) => {
-        state.Status = "sendcode requested"
+        state.Status = "sending the code requested"
     },
     [LOGIN_REQUEST_SENDCODE_SUCCESS]: (state) => {
-        state.Status = "sendcode success"
+        state.Status = "sending the code successfully"
     },
     [LOGIN_REQUEST_SENDCODE_ERROR]: (state, payload) => {
-        state.Status = "sendcode error"
+        state.Status = "sending the code failed"
+        if (payload && payload.response && payload.response.data && payload.response.data.error) {
+            Helpers.notify("is-danger", payload.response.data.error)
+        }
+        // else Helpers.notify("is-danger", i18n.t("Message.Backend.Default"))
+    },
+    [LOGIN_REQUEST_TOKEN]: (state) => {
+        state.Status = "receiving the token requested"
+    },
+    [LOGIN_REQUEST_TOKEN_SUCCESS]: (state, payload) => {
+        state.Status = "receiving the token successfully"
+        try {
+            state.Token = `Bearer ${payload.token}`
+            state.ExpirationDate = payload.expirationDate
+            // const ExpireDate = new Date(state.ExpirationDate).toString()
+            // window.$cookies.set("AuthToken", state.Token, ExpireDate)
+            // window.$cookies.set("ExpirationDate", state.ExpirationDate, ExpireDate)
+            HTTP.defaults.headers.common.Authorization = state.Token
+            state.Status = "auth is complete"
+        }
+        catch (error) {
+            state.Status = `auth error: ${error}`
+        }
+    },
+    [LOGIN_REQUEST_TOKEN_ERROR]: (state, payload) => {
+        state.Status = "receiving the token failed"
         if (payload && payload.response && payload.response.data && payload.response.data.error) {
             Helpers.notify("is-danger", payload.response.data.error)
         }
--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -11,19 +11,28 @@
                     <div v-if="!CodeSent" class="field">
                         <label class="label">Введите ваш номер телефона</label>
                         <div class="control">
-                            <input class="input" type="text" placeholder="Номер телефона">
+                            <input v-model="Phone" class="input" type="text" placeholder="Номер телефона">
                         </div>
                     </div>
                     <div v-else class="field">
                         <label class="label">Введите код</label>
                         <div class="control">
-                            <input class="input" type="text" placeholder="Код">
+                            <input v-model="Code" class="input" type="text" placeholder="Код">
                         </div>
                     </div>
                     <div class="control">
                         <button
+                            v-if="!CodeSent"
                             type="submit"
                             @click.prevent="sendcode()"
+                            class="button is-theme is-fullwidth"
+                        >
+                            Войти
+                        </button>
+                        <button
+                            v-else
+                            type="submit"
+                            @click.prevent="login()"
                             class="button is-theme is-fullwidth"
                         >
                             Войти
@@ -36,7 +45,7 @@
 </template>
 
 <script>
-import { LOGIN_REQUEST_SENDCODE } from "@/store/actions/auth"
+import { LOGIN_REQUEST_SENDCODE, LOGIN_REQUEST_TOKEN } from "@/store/actions/auth"
 
 export default {
     name: "Login",
@@ -52,6 +61,16 @@ export default {
             this.$store.dispatch(LOGIN_REQUEST_SENDCODE, this.Phone)
                 .then(() => {
                     this.CodeSent = !this.CodeSent
+                })
+                // .finally(() => this.switchLoading())
+        },
+        login() {
+            this.$store.dispatch(LOGIN_REQUEST_TOKEN, {
+                Phone: this.Phone,
+                Code: this.Code
+            })
+                .then(() => {
+                    alert("Success")
                 })
                 // .finally(() => this.switchLoading())
         }

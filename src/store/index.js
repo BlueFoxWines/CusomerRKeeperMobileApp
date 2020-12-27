978458ce13b1b63c152ac85d74fc9cbf02387cc6
Anton Zolotov <antonzolotov@outlook.com>
2020-12-27 16:16:55 +0500

    Реализовано хранение данных в localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from "vuex"
+import createPersistedState from "vuex-persistedstate"
 
 import Auth from "./modules/auth"
 
@@ -6,7 +7,8 @@ const store = createStore({
     strict: process.env.NODE_ENV !== "production",
     modules: {
         Auth
-    }
+    },
+    plugins: [createPersistedState()]
 })
 
 export default store

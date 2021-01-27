db55aa46bedd16132412993e9510664a6e2326cb
Yuliya-Sinitskaya <78029400+Yuliya-Sinitskaya@users.noreply.github.com>
2021-01-27 17:10:47 +0300

    Изменен текст кнопки
    Текст кнопки

--- src/views/Menu/Index.vue
+++ src/views/Menu/Index.vue
@@ -33,6 +33,7 @@
                         <figure class="image is-128x128">
                             <img src="https://bulma.io/images/placeholders/128x128.png">
                         </figure>
+                        Подпись
                     </div>
                     <div class="menu-item column is-half">
                         <figure class="image is-128x128">

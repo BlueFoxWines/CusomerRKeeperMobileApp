c515ef418960be40c4778a9e3ac140a48d9602af
Anton Zolotov <antonzolotov@outlook.com>
2021-08-24 13:46:28 +0500

    Бамп версий

--- android/app/src/main/assets/public/cordova_plugins.js
+++ android/app/src/main/assets/public/cordova_plugins.js
@@ -10,6 +10,14 @@
         ]
         },
       {
+          "id": "cordova-plugin-flashlight.Flashlight",
+          "file": "plugins/cordova-plugin-flashlight/www/Flashlight.js",
+          "pluginId": "cordova-plugin-flashlight",
+        "clobbers": [
+          "window.plugins.flashlight"
+        ]
+        },
+      {
           "id": "cordova-plugin-ionic.guards",
           "file": "plugins/cordova-plugin-ionic/dist/guards.js",
           "pluginId": "cordova-plugin-ionic",
@@ -19,6 +27,7 @@
     module.exports.metadata =
     // TOP OF METADATA
     {
+      "cordova-plugin-flashlight": "3.2.0",
       "cordova-plugin-ionic": "5.4.7",
       "cordova-plugin-whitelist": "1.3.4"
     };
